878182cdb4650b103265b57e6e4ed7ad23cd17eb
NikitaPilez <pileznikita99@gmail.com>
2019-09-16 12:47:10 +0300

    TravelBlog-3: Make module auth.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', 'BlogController@index');
 
 Route::get('/category/{categoryName}','BlogController@getCategory');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
