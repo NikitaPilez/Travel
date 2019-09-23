68cb5c818e3f405112d701ba015ee960a450eba1
NikitaPilez <pileznikita99@gmail.com>
2019-09-24 00:24:13 +0300

    Displayed star posts on the post page.

--- public/js/post.js
+++ public/js/post.js
@@ -6,14 +6,19 @@
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
+ 
     $('.js-star-post').click(function (event) {
         var elem = $(this);
         var postId = elem.attr('data-post-id');
+        // var countStar = elem.attr('data-count-star');
         var actionStar = 'put';
-
+        var starContent = elem.parent().html();
+        
         if($(this).hasClass('fas')) {
             actionStar = 'delete';
+            elem.removeClass('fas').addClass('far');
+        } else {
+             elem.removeClass('far').addClass('fas');
         }
 
         $.ajax({
@@ -21,11 +26,7 @@
             url: 'put-star',
             data: {postId: postId, actionStar: actionStar},
             success: function (data) {
-                if(data.hasStar){
-                    elem.removeClass('far').addClass('fas');
-                } else {
-                    elem.removeClass('fas').addClass('far');
-                }
+                //changed count star.                
             },
         });
     });
