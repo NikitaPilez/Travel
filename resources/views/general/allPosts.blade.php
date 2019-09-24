63919612c5f27247454c5c3e5e26a72ca8c3b43a
NikitaPilez <pileznikita99@gmail.com>
2019-09-24 12:18:34 +0300

    TravelBlog-17: Made alignment user avatar and name in posts card.

--- resources/views/general/allPosts.blade.php
+++ resources/views/general/allPosts.blade.php
@@ -12,9 +12,9 @@
                     <a href="{{asset('category/'.$post->category->name)}}">{{$post->category->name}}</a>
                 </div>
                 <h1 class="item-entry__title"><a href="{{asset('post/'.$post->id)}}">{{$post->header}}</a></h1>
-                <a href="{{asset('user/'.$post->user->name)}}" class="entry__profile-pic">
-                    <img class="user-avatar" src="https://picsum.photos/id/{{$post->id}}/50/50" alt="">
-                    <h6 class="d-inline-block">{{$post->user->name}}</h6>
+                <a href="{{asset('user/'.$post->user->id)}}" class="entry__profile-pic d-flex justify-content-center">
+                    <img class="user-avatar align-items-center" src="https://picsum.photos/id/{{$post->id}}/50/50" alt="">
+                    <p class="align-items-center font-weight-bold margin-left-username">{{$post->user->name}}</p>
                 </a>
                 <div class="item-entry__date">
                     <a href="{{asset('post/'.$post->id)}}">{{ \Carbon\Carbon::parse($post->created_at)->format('d F Y')}}</a>
