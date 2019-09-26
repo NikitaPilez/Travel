e797dc932a31a5a0062daf362b14a6d373b8d394
NikitaPilez <pileznikita99@gmail.com>
2019-09-26 17:41:16 +0300

    TravelBlog-23: Create user profile page.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,14 +5,20 @@
 @endsection
 @section('content')
 <div class="container w-100 h-100 margin-top-150 background-color-user-profile">
-    <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10 text-center background-color-white">
-            <img class="user-profile-avatar" src="https://picsum.photos/id/{{$user->id}}/300/300" alt="">
-            <h3>df</h3>
-            <h3>df</h3>
+        <div style="height: 400px;background-image: url('https://picsum.photos/id/120/1200/400')">
+            <div class="col-md-6">
+                <div class="d-flex align-items-center">
+                    <div>
+                        <img class="user-profile-avatar" src="https://picsum.photos/id/3/150/150" alt="">
+                    </div>
+                    <div>
+                       <h1 class="color-white">Nikita Pilets</h1>
+                       <h3 class="color-white">Belarus, Minsk</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6"></div>
         </div>
-    </div>
 </div>
 @endsection
 
