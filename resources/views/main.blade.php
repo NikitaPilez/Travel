1f419a717f4fbb8a3470a376c4a2c5a0a93e1049
NikitaPilez <pileznikita99@gmail.com>
2019-09-30 15:10:28 +0300

    TravelBlog-23: Deleted template, extra styles,scripts, downloaded bootstrap in project.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,32 +7,17 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!-- <link rel="stylesheet" href="{{asset('css/base.css')}}">
-    <link rel="stylesheet" href="{{asset('css/vendor.css')}}">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}"> -->
     <link rel="stylesheet" href="{{asset('css/general-styles.css')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap-grid.css')}}">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="{{asset('js/modernizr.js')}}"></script>
-    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
-    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     @yield('assets')
 </head>
-<body id="top" class="bg-body">
-    <div id="preloader">
-        <div id="loader" class="dots-fade">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
+<body class="bg-body">
 
     @yield('content')
 
     <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
-    <script src="{{asset('js/plugins.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
     <script src="{{asset('js/post.js')}}"></script>
 </body>
 </html>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,104 +1,45 @@
 @extends('main')
 @section('content')
-    <section class="s-content s-content--narrow">
-        <article class="row entry format-standard">
-            <div class="entry__header col-full">
-                <h3 class="entry__header-title ">
-                   {{$post->header}}
-                </h3>
-                <ul class="entry__header-meta d-flex justify-content-center align-items-center">
-                    <img class="user-avatar" href="{{asset('user/'.$post->user->id)}}" src="https://picsum.photos/id/{{$post->id}}/50/50" alt="">
-                    <a class="margin-left-username" href="{{asset('user/'.$post->user->id)}}">{{$post->user->name}}</a>
-                    <li class="date">{{ \Carbon\Carbon::parse($post->created_at)->format('d F Y')}}</li>
-                </ul>
-            </div>
-            <div class="col-full text-center">
-                <div class="entry__post-thumb">
-                    <img src="https://picsum.photos/id/{{$post->id}}/900/400"
-                         alt="">
-                </div>
-                <div class="font-size-star color-gray">
-                    <p class="d-inline-block"> {{count($post->comments)}} </p>
-                    <i class="fa fa-comment"></i>
-                    <p class="js-count-star d-inline-block">{{count($post->stars)}}</p>
-                    @if($post->currentUserIsLike())
-                    <i class="cursor-pointer fas fa-star js-star-post" data-post-id="{{$post->id}}"></i>
-                    @else
-                    <i class="cursor-pointer far fa-star js-star-post" data-post-id="{{$post->id}}"></i>
-                    @endif
-                </div>
-            </div>
-            <div class="col-full">
-                {!! $post->body !!}
-            </div>
-            <div class="entry__taxonomies">
-                <div class="entry__cat">
-                    <h5>Posted In:
-                        <a href="{{asset('category/'.$post->category->name)}}">{{$post->category->name}}</a>
-                    </h5>
-                </div>
-                <div class="entry__tags">
-                    <h5>Tags: </h5>
-                    <span class="entry__tax-list entry__tax-list--pill">
-                        @foreach($post->tags as $tag)
-                            <a href="{{asset('tag/'.$tag->name)}}">{{$tag->name}}</a>
-                        @endforeach
-                    </span>
-                </div>
-            </div>
-        </article>
 
-        <div class="s-content__entry-nav">
-            <div class="row s-content__nav">
-                <div class="col-six s-content__prev">
-                    <a href="{{asset('post/'.$previousPost->id)}}" rel="prev">
-                        <span>Previous Post</span>
-                        {{$previousPost->header}}
-                    </a>
+    <div class="container mt-100">
+        <div class="row d-flex justify-content-center">
+            <h1> {{$post->header}} </h1>
+            <div class="d-flex align-items-center justify-content-center mt-2">
+                <h5 class="mr-2 text-black-50"> {{ \Carbon\Carbon::parse($post->created_at)->format('d F Y')}} </h5>
+                <div class="mb-1">
+                    By <img class="user-avatar ml-1" href="{{asset('user/'.$post->user->id)}}"
+                            src="https://picsum.photos/id/{{$post->id}}/50/50" alt="">
+                    <a class="margin-left-username text-uppercase font-weight-bold" href="{{asset('user/'.$post->user->id)}}">{{$post->user->name}}</a>
                 </div>
-                <div class="col-six s-content__next">
-                    <a href="{{asset('post/'.$nextPost->id)}}" rel="next">
-                        <span>Next Post</span>
-                        {{$nextPost->header}}
-                    </a>
-                </div>
+            </div>
+            <div class="mt-4 mb-4">
+                <img src="https://picsum.photos/id/{{$post->id}}/900/400" alt="">
             </div>
         </div>
-
-        <div class="comments-wrap">
-            <div id="comments" class="row">
-                <div class="col-full">
-                    <h3 class="h2">{{count($post->comments)}} comments</h3>
-                    <ol class="commentlist">
-                        @foreach($post->comments as $comment)
-                        <li class="depth-1 comment">
-                            <div class="comment__avatar">
-                                <a href="{{asset('user/'.$comment->user->id)}}">
-                                    <img class="avatar" src="https://picsum.photos/id/{{$comment->id}}/50/50" alt="" width="50" height="50">
-                                </a>
-                            </div>
-                            <div class="comment__content">
-                                <div class="comment__info">
-                                    <a href="{{asset('user/'.$comment->user->id)}}">
-                                        <div class="comment__author">{{$comment->user->name}}</div>
-                                    </a>
-                                    <div class="comment__meta">
-                                        <div class="comment__time">{{\Carbon\Carbon::parse($comment->created_at)->format('d F Y')}}</div>
-                                        <div class="comment__reply">
-                                            <a class="comment-reply-link" href="#0">Reply</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="comment__text">
-                                    <p>{{$comment->body}}</p>
-                                </div>
-                            </div>
-                        </li>
-                        @endforeach
-                    </ol>
-                </div>
-            </div>
+        <div class="d-flex justify-content-center align-items-center">
+            <h3 class="d-inline-block m-1"> {{count($post->comments)}} </h3>
+            <i class="fa fa-comment font-size-15"></i>
+            <h3 class="js-count-star d-inline-block m-1">{{count($post->stars)}} </h3>
+            @if($post->currentUserIsLike())
+                <i class="cursor-pointer fa fa-star js-star-post font-size-15" data-post-id="{{$post->id}}"></i>
+            @else
+                <i class="cursor-pointer fa fa-star-o js-star-post font-size-15" data-post-id="{{$post->id}}"></i>
+            @endif
         </div>
-        @include('general.popularPosts')
-    </section>
+        <div class="row m-3">
+            <h3>Tags:</h3>
+            @foreach($post->tags as $tag)
+                <div class="d-inline-block m-1">
+                    <a class="border-tags color-gray" href="{{asset('tag/'.$tag->name)}}">{{$tag->name}}</a>
+                </div>
+            @endforeach
+            <h3 class="d-block">Category: <a class="text-uppercase text-black-50" href="{{asset('category/'.$post->category->name)}}">{{$post->category->name}}</a></h3>
+        </div>
+        <div class="m-3 font-size-15">
+            {!! $post->body !!}
+        </div>
+{{--        Comments--}}
+    </div>
+
+    @include('general.popularPosts')
 @endsection
