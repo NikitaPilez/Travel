4647258c98c35fd1d8cec6aaf4e4a2b2a44b82d5
NikitaPilez <pileznikita99@gmail.com>
2019-10-09 12:17:28 +0300

    TravelBlog-28: Create navigation panel.

---
+++ resources/views/general/nav.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-black">
+    <div class="container">
+        <a class="navbar-brand text-white" href="{{asset('/')}}">Travel</a>
+        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto w-100 d-flex justify-content-center align-items-center">
+                <li class="nav-item">
+                    <a class="navigate-link m-2" href="{{asset('/')}}">Home</a>
+                </li>
+                <li >
+                    <a class="navigate-link m-2" href="{{asset('/')}}">Blog</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
