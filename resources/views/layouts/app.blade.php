800fc26ef5aa773b4843c1f2d707530f1ef0f37e
Nikita Pilez <pileznikita99@gmail.com>
2019-11-01 16:50:51 +0300

    TravelBlog-32: Made a displaying child comments of post, box shadow for blocks.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu shadow-classic">
+                                <ul class="dropdown-menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
