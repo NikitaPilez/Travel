b5d739d4b380b97f7df05fcafa5002e8fdfb2f85
NikitaPilez <pileznikita99gmail.com>
2020-04-29 21:15:11 +0300

    TravelBlog-49: Created login through Google.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable();
             $table->rememberToken();
             $table->string('created_at');
             $table->string('updated_at')->nullable();
